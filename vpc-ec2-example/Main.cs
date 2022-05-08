e2a61717f40c83a3fbe5b712e62997e628312bec
Peer Jakobsen <peerjako@amazon.com>
2022-05-08 17:48:13 +0200

    Testing ec2 with session manager

--- vpc-ec2-example/Main.cs
+++ vpc-ec2-example/Main.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using HashiCorp.Cdktf.Providers.Aws;
 using HashiCorp.Cdktf.Providers.Aws.Ec2;
 using HashiCorp.Cdktf.Providers.Aws.Vpc;
+using HashiCorp.Cdktf.Providers.Aws.Iam;
 
 namespace MyCompany.MyApp
 {
@@ -15,25 +16,37 @@ namespace MyCompany.MyApp
         {
             // define resources here
             new AwsProvider(this, "AWS", new AwsProviderConfig { Region = "eu-north-1" });
-
-            new AwsProvider(this, "AWS", new AwsProviderConfig { Region = "eu-north-1" });
             
             Vpc vpc = new Vpc(this, "vpc", new VpcConfig{
                 CidrBlock = "10.0.0.0/16",
-                Tags = new Dictionary<string, string>{{"Name","vpc-ec2-example"}}
+                EnableDnsHostnames = true,
+                EnableDnsSupport = true,
+                Tags = new Dictionary<string, string>{{"Name","vpc-ec2-trading-hub"}}
             });
             
             Subnet subnet = new Subnet(this, "subnet", new SubnetConfig{
                 VpcId = vpc.Id,
                 CidrBlock = "10.0.10.0/24",
                 AvailabilityZone = "eu-north-1a",
-                Tags = new Dictionary<string, string>{{"Name","vpc-ec2-example"}}
+                Tags = new Dictionary<string, string>{{"Name","vpc-ec2-trading-hub"}}
             });
+
+            sessionmanager.Sessionmanager ssm = new sessionmanager.Sessionmanager(this, "ssm", new sessionmanager.SessionmanagerOptions{
+                BucketName = "my-session-logs-peerjako",
+                AccessLogBucketName = "my-session-access-logs-peerjako",
+                VpcId = vpc.Id,
+                EnableLogToS3 = true,
+                EnableLogToCloudwatch = true,
+                VpcEndpointsEnabled = true,
+                VpcEndpointPrivateDnsEnabled = true,
+                SubnetIds = new string[]{subnet.Id},
+            });
+
 
             NetworkInterface networkInterface = new NetworkInterface(this, "ec2-network-interface", new NetworkInterfaceConfig{
                 SubnetId = subnet.Id,
                 PrivateIp = "10.0.10.100",
-                Tags = new Dictionary<string, string>{{"Name","vpc-ec2-example"}}
+                Tags = new Dictionary<string, string>{{"Name","vpc-ec2-trading-hub"}}
             });
             
             DataAwsAmi latestAmazonLinux2Ami = new DataAwsAmi(this, "latest-ami", new DataAwsAmiConfig{
@@ -42,11 +55,64 @@ namespace MyCompany.MyApp
                 Filter = new DataAwsAmiFilter[]{
                     new DataAwsAmiFilter{
                         Name = "name",
-                        Values = new string[]{"amzn-ami-hvm-*-x86_64-gp2"}
+                        Values = new string[]{"amzn2-ami-hvm-*-x86_64-gp2"}
                     }
                 }
             });
 
+            DataAwsIamPolicyDocumentStatement statement = new DataAwsIamPolicyDocumentStatement{
+                Actions = new string[]{"ssm:UpdateInstanceInformation",
+                "ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel",
+                "s3:GetEncryptionConfiguration"},
+                Effect = "Allow",
+                Resources = new string[]{"*"}
+            };
+
+            DataAwsIamPolicyDocument policyDoc = new DataAwsIamPolicyDocument(this, "iam-policy-doc", new DataAwsIamPolicyDocumentConfig
+            {
+                Statement = new DataAwsIamPolicyDocumentStatement[]{statement}
+            });
+            IamPolicy iamPolicy = new IamPolicy(this, "iam-policy", new IamPolicyConfig{
+                Policy = policyDoc.Json,
+                NamePrefix = "vpc-ec2-trading-hub-"
+            });
+
+            DataAwsIamPolicyDocumentStatementPrincipals principals = new DataAwsIamPolicyDocumentStatementPrincipals{
+                Type = "Service",
+                Identifiers = new string[]{"ec2.amazonaws.com"}
+            };
+            DataAwsIamPolicyDocumentStatement assumeStatement = new DataAwsIamPolicyDocumentStatement{
+                Actions = new string[]{"sts:AssumeRole"},
+                Principals = new DataAwsIamPolicyDocumentStatementPrincipals[]{ principals },          
+                Effect = "Allow"
+            };
+
+            DataAwsIamPolicyDocument assumePolicyDoc = new DataAwsIamPolicyDocument(this, "iam-assume-policy-doc", new DataAwsIamPolicyDocumentConfig
+            {
+                Statement = new DataAwsIamPolicyDocumentStatement[]{assumeStatement}
+            });
+
+            IamRole iamRole = new IamRole(this, "iam-role", new IamRoleConfig{
+                AssumeRolePolicy = assumePolicyDoc.Json
+            });
+            IamRolePolicyAttachment rolePolicyAttachment = new IamRolePolicyAttachment(this, "rpa", new IamRolePolicyAttachmentConfig{
+                Role = iamRole.Name,
+                PolicyArn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+            });
+            IamInstanceProfile instanceProfile = new IamInstanceProfile(this, "instance-profile", new IamInstanceProfileConfig{
+                Role = iamRole.Name
+            });
+
+            //ssh-keygen -t ed25519 -m PEM -C "youremail@company.com" -f $HOME/.ssh/ec2_id_ed25519; chmod 400 ~/.ssh/ec2_id_ed25519* 
+            string pubKey = System.IO.File.ReadAllText(System.IO.Path.Combine(System.Environment.GetEnvironmentVariable("HOME"), ".ssh", "ec2_id_ed25519.pub")).Replace(Environment.NewLine, "");
+            
+            KeyPair keyPair = new KeyPair(this, "ec2-keypair", new KeyPairConfig{
+                KeyNamePrefix = "trading-hub-kp-",
+                PublicKey = pubKey
+            });
             Instance instance = new Instance(this, "compute", new InstanceConfig
             { 
                 Ami = latestAmazonLinux2Ami.Id, //"ami-01ded35841bc93d7f", //latestAmazonLinux2Ami.ImageId,
@@ -57,7 +123,9 @@ namespace MyCompany.MyApp
                         NetworkInterfaceId = networkInterface.Id
                     }
                 },
-                Tags = new Dictionary<string, string>{{"Name","vpc-ec2-example"}}
+                KeyName = keyPair.KeyName,
+                IamInstanceProfile = instanceProfile.Name,
+                Tags = new Dictionary<string, string>{{"Name","vpc-ec2-trading-hub"}}
             });
 
             new TerraformOutput(this, "private_ip", new TerraformOutputConfig
--- vpc-ec2-example/README.md
+++ vpc-ec2-example/README.md
@@ -1,7 +1,3 @@
----
-typora-copy-images-to: ../vpc-ec2-example
----
-
 # VPC with EC2 instance Terraform for CDK example
 
 The Cloud Development Kit for Terraform (CDKTF) allows you to define your infrastructure in a familiar programming language such as TypeScript, Python, Go, C#, or Java.
